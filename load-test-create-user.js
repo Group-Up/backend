12f695e6e4f45c3641a7925f6b73dbf9ddddf178
zachary schumpert <buphnezz@gmail.com>
2018-06-21 18:23:02 -0700

    loadtest functioning

--- load-test-create-user.js
+++ load-test-create-user.js
@@ -7,7 +7,7 @@ const loadTestUser = module.exports = {};
 loadTestUser.create = (userContext, events, done) => {
   userContext.vars.username = faker.internet.userName() + Math.random() + Math.random();
   userContext.vars.email = faker.internet.email();
-  userContext.vars.passwordHash = faker.internet.password() + Math.random() + Math.random();
+  userContext.vars.password = faker.internet.password() + Math.random() + Math.random();
 
   return done();
 };
